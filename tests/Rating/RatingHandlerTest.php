ffedc3acc134cdc54a3e4691534a27c2143854d0
zacube <contact@zacube.com>
2026-06-05 09:55:35 +0200

    Ajout de testAverageReviewScoreCalculation

--- tests/Rating/RatingHandlerTest.php
+++ tests/Rating/RatingHandlerTest.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Tests\Rating;
+use App\Model\Entity\Review;
 use App\Model\Entity\VideoGame;
 use App\Rating\RatingHandler;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -28,5 +29,28 @@ class RatingHandlerTest extends TestCase
 
         $this->ratingHandler->calculateAverage($vg);
     }
-//public function testAverageReviewScoreCalculation()
+    public function testAverageReviewScoreCalculation(){
+        $reviews = new ArrayCollection();
+        $review1 = $this->getMockBuilder(Review::class)->getMock();
+        $review1
+            ->method('getRating')
+            ->willReturn(2);
+        $review2 = $this->getMockBuilder(Review::class)->getMock();
+        $review2
+            ->method('getRating')
+            ->willReturn(4);
+        $reviews->add($review1);
+        $reviews->add($review2);
+
+        $vg = $this->getMockBuilder(VideoGame::class)->getMock();
+        $vg
+            ->method('getReviews')
+            ->willReturn($reviews);
+        $vg
+            ->expects($this->once())
+            ->method('setAverageRating')
+            ->with(3);
+
+        $this->ratingHandler->calculateAverage($vg);
+    }
 }
