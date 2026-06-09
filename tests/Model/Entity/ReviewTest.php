4d228f469813a43f4b698abb6d5d3747655c3351
zacube <contact@zacube.com>
2026-06-09 22:11:15 +0200

    ReviewTest.php : test unitaire pour validation de la note via l'entity Review

---
+++ tests/Model/Entity/ReviewTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Tests\Model\Entity;
+
+use App\Model\Entity\Review;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Validation;
+
+class ReviewTest extends TestCase
+{
+    public static function invalidRatingsProvider(): iterable
+    {
+        yield 'note trop basse' => [0];
+        yield 'note trop haute' => [6];
+    }
+
+    /**
+     * @dataProvider invalidRatingsProvider
+     */
+    public function testInvalidNotes(int $rating)
+    {
+        $validator = Validation::createValidatorBuilder()
+            ->enableAttributeMapping() // pour lire #[Assert\...]
+            ->getValidator(); // renvoie un objet Validator
+
+        $review = new Review();
+        $review->setRating($rating);
+
+        $violations = $validator->validate($review);
+
+        $this->assertCount(1, $violations); // on attend 1 erreur de validation
+    }
+
+    public static function validRatingsProvider(): iterable
+    {
+        yield 'note ok1' => [1];
+        yield 'note ok3' => [3];
+        yield 'note ok5' => [5];
+    }
+
+    /**
+     * @dataProvider validRatingsProvider
+     */
+    public function testValidNotes(int $rating)
+    {
+        $validator = Validation::createValidatorBuilder()
+            ->enableAttributeMapping()
+            ->getValidator();
+
+        $review = new Review();
+        $review->setRating($rating);
+
+        $violations = $validator->validate($review);
+
+        $this->assertCount(0, $violations); // on n'attend pas d'erreur de validation
+    }
+}
