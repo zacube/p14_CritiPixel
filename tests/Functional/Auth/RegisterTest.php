e0a40017300845311f76b9d9868865c56f62e08a
zacube <contact@zacube.com>
2026-06-10 20:10:27 +0200

    Dernière correction PhpStan + PHP CS Fixer

--- tests/Functional/Auth/RegisterTest.php
+++ tests/Functional/Auth/RegisterTest.php
@@ -18,7 +18,7 @@ final class RegisterTest extends FunctionalTestCase
 
         self::assertResponseRedirects('/auth/login');
 
-        $user = $this->getEntityManager()->getRepository(User::class)->findOneByEmail('user@email.com');
+        $user = $this->getEntityManager()->getRepository(User::class)->findOneBy(['email' => 'user@email.com']);
 
         $userPasswordHasher = $this->service(UserPasswordHasherInterface::class);
 
@@ -30,6 +30,8 @@ final class RegisterTest extends FunctionalTestCase
 
     /**
      * @dataProvider provideInvalidFormData
+     *
+     * @param array<string, string> $formData
      */
     public function testThatRegistrationShouldFailed(array $formData): void
     {
